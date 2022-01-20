c7da5d947c221967f0c36b4b83527ef88f83877c
justiniantheemperor <justinrellis52@gmail.com>
2022-01-19 23:46:53 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission3.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GradingCalculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GradingCalculator (GradingCalculatorModel model)
+        {
+            return View();
+        }
+
+    }
+}
